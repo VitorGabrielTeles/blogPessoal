deb1de4c7f0e239da9488badd7d732a6f4e3d42c
Vitor Teles <vitorgabrielteles2301@gmail.com>
2025-04-14 19:45:40 -0300

    Projeto Blog Pessoal

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,58 @@
+package com.generation.blogpessoal.model;
+import java.time.LocalDate;
+import org.hibernate.annotations.UpdateTimestamp;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Cria o id atomaticamnete
+	private Long id;
+	
+	@NotBlank //Nao valide valores nulos
+	@Size(min = 3, max = 100) // Valida 
+	private String titulo;
+	
+	@NotBlank //Nao valide valores nulos
+	@Size(min = 3, max = 100) // Valida 
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDate data;
+	
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+}
